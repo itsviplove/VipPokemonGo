8842a75819bcf06bd2c47078fb55981408fb9add
keshav garg <keshavgarg@keshavs-MacBook-Pro.local>
2017-03-14 22:46:14 +0530

    Initial Commit

---
+++ VipPokemonGo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VipPokemonGo
+//
+//  Created by keshav garg on 14/03/17.
+//  Copyright © 2017 Viplove bansal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

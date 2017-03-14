4cad044fbc7d052a092fe282ef8e1728967a8abd
Viplove Bansal <keshavgarg@keshavs-MacBook-Pro.local>
2017-03-15 01:35:47 +0530

    Map n user location

--- VipPokemonGo/ViewController.swift
+++ VipPokemonGo/ViewController.swift
@@ -7,19 +7,68 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController , CLLocationManagerDelegate {
+    
+    
+    @IBOutlet weak var mapView: MKMapView!
+    var manager = CLLocationManager()
+    var updateCount = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        manager.delegate = self
+        
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            print("ready to go")
+            mapView.showsUserLocation = true
+            manager.startUpdatingLocation()
+            Timer.scheduledTimer(withTimeInterval: 5, repeats: true, block: { (Timer) in
+                if let cord = self.manager.location?.coordinate {
+                    print("see")
+                    let anno = MKPointAnnotation()
+                    anno.coordinate = cord
+                    let randoLat = (Double(arc4random_uniform(200)) - 100.0) / 50000.0
+                    let randoLon = (Double(arc4random_uniform(200)) - 100.0) / 50000.0
+                    anno.coordinate.latitude += randoLat
+                    anno.coordinate.longitude += randoLon
+                    self.mapView.addAnnotation(anno)
+                }})
+            
+            
+        } else {
+            manager.requestWhenInUseAuthorization()
+        }
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print("lets")
+        if updateCount < 3 {
+            let region = MKCoordinateRegionMakeWithDistance(manager.location!.coordinate, 200, 200)
+            
+            mapView.setRegion(region, animated: false)
+            
+            updateCount += 1
+        }
+        
     }
-
-
+    
+    @IBAction func centreTapped(_ sender: Any) {
+        
+        if let cord = manager.location?.coordinate {
+            let region = MKCoordinateRegionMakeWithDistance(cord, 200 , 200)
+            
+            mapView.setRegion(region, animated: false)
+        }
+    }
+    
+    
+    
+    
+    
 }
 
